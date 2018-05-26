a06831bc670aa60c4a1823c089d082bd1c75dcf5
Joey Biotti <joey@MacBook-Pro-2.local>
2018-05-26 00:07:56 -0500

    javascripting stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,35 @@
-document.getElementById('output').innerHTML = new Date();
+let btn = document.getElementById('btn');
+const output = document.getElementById('output');
 
 document.write('The output will be supplied via JSON. Everything will be JavaScipt in this bitch.');
 
-const bands = [
-	{
-		name:"Bongripper",
-		description:"Satanic Doom Metal",
-	},
-	{
-		name:"Weedeater",
-		description:"North Carolina Stoner Metal",
-	},
-	{
-		name:"Bongzilla",
-		description:"666lb. Bong Session"
-	},
-	{
-		name:"Sleep",
-		description:"The OGs."
-	},
+function seeStonerMetal() {
+
+	moreText = `<div>
+					<h2>This click shit works.</h2>
+				</div>`
+
+	output.innerHTML += moreText
+};
+
+
+const bands = [{
+        name: "Bongripper",
+        description: "Satanic Doom Metal",
+    },
+    {
+        name: "Weedeater",
+        description: "North Carolina Stoner Metal",
+    },
+    {
+        name: "Bongzilla",
+        description: "666lb. Bong Session"
+    },
+    {
+        name: "Sleep",
+        description: "The OGs."
+    },
 ];
 
-const output = document.getElementById('output');
+btn.addEventListener("click", seeStonerMetal);
 
-for(band in bands){
-	let cards = `<section>
-					<div>
-						<h1>${bands[band].name}<h1>
-						<p>${bands[band].description}</p>
-						<hr>
-					</div>
-				</section>`
-
-output.innerHTML += cards;
-
-};
